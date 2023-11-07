1b5b853247afca270301e7c501f9c0a182098504
Mauliyak <148433945+Mauliyak@users.noreply.github.com>
2023-11-07 23:24:26 +0700

    Add files via upload

---
+++ Polliklinik/koneksi.php
@@ -0,0 +1,8 @@
+<?php 
+$databaseHost = 'localhost';
+$databaseName = 'polliklinik';
+$databaseUsername = 'root';
+$databasePassword = '';
+ 
+$mysqli = mysqli_connect($databaseHost, 
+    $databaseUsername, $databasePassword, $databaseName);
