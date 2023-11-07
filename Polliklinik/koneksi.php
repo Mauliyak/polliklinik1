6e81ef7fd263a3d53d0b3b571a34c4bb671177ef
Mauliyak <148433945+Mauliyak@users.noreply.github.com>
2023-11-07 23:21:58 +0700

    Delete Polliklinik directory

--- Polliklinik/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$databaseHost = 'localhost';
-$databaseName = 'polliklinik';
-$databaseUsername = 'root';
-$databasePassword = '';
- 
-$mysqli = mysqli_connect($databaseHost, 
-    $databaseUsername, $databasePassword, $databaseName);
